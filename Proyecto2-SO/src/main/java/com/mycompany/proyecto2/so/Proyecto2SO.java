f53b3c23b42061c755d0f6ca79e99e7d1424f6c7
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2024-05-01 17:39:18 -0600

    Clases MMU, Pagina, Proceso y simulacion
    Contiene errores es MMU

---
+++ Proyecto2-SO/src/main/java/com/mycompany/proyecto2/so/Proyecto2SO.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.proyecto2.so;
+
+/**
+ *
+ * @author rebecamadrigal
+ */
+public class Proyecto2SO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Proyecto2-SO/src/main/java/com/mycompany/proyecto2/so/Simulacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto2.so;
+
+/**
+ *
+ * @author rebecamadrigal
+ */
+import java.util.Scanner;
+
+public class Simulacion {
+    private int semilla;
+    private String algoritmoSeleccionado;
+    private String archivoInstrucciones;
+    private int numProcesos;
+    private int numOperaciones;
+    private MMU mmu;
+    private AlgoritmoPaginacion algoritmoSeleccionado;
+
+    // Métodos para inicializar la simulación y la interfaz de usuario
+    public static void main(String[] args) {
+
+    }
+}
+
