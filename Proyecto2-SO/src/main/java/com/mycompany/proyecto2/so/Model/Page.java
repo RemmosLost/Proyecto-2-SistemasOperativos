b0b1df81dc0f5fc4eff6bd0387a4d6157567e480
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2024-05-01 21:48:43 -0600

    Page

--- Proyecto2-SO/src/main/java/com/mycompany/proyecto2/so/Model/Page.java
+++ Proyecto2-SO/src/main/java/com/mycompany/proyecto2/so/Model/Page.java
@@ -9,9 +9,47 @@ package com.mycompany.proyecto2.so.Model;
  * @author rebecamadrigal
  */
 public class Page {
-    
-    
-    
-    
-}
+    private int pageID;
+    private String phyAdress;
+    private boolean flag;
+    private boolean algorithm;
 
+    public Page(int pageID, String phyAdress, boolean flag, boolean algorithm) {
+        this.pageID = pageID;
+        this.phyAdress = phyAdress;
+        this.flag = flag;
+        this.algorithm = algorithm;
+    }
+
+    public int getPageID() {
+        return pageID;
+    }
+
+    public void setPageID(int pageID) {
+        this.pageID = pageID;
+    }
+
+    public String getPhyAdress() {
+        return phyAdress;
+    }
+
+    public void setPhyAdress(String phyAdress) {
+        this.phyAdress = phyAdress;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public boolean isAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(boolean algorithm) {
+        this.algorithm = algorithm;
+    }
+}
